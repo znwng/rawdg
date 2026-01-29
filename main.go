193b01d65215785dc047045e0c0d70c587b227c1
znwng <kalyan3x90@gmail.com>
2026-01-29 16:42:36 +0530

    Add smooth progress bar animation

--- main.go
+++ main.go
@@ -2,21 +2,64 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"os"
 	"path/filepath"
+	"strings"
 	"time"
 
-	"rawdg/config" // Custom package to load config
-	"rawdg/sound"  // Custom package to play sound
+	"rawdg/config"
+	"rawdg/sound"
+
+	"golang.org/x/term"
 )
+
+// Function to get terminal width
+func getTerminalWidth() int {
+	if !term.IsTerminal(int(os.Stdout.Fd())) {
+		return 80
+	}
+	w, _, err := term.GetSize(int(os.Stdout.Fd()))
+	if err != nil || w <= 0 {
+		return 80
+	}
+	return w
+}
+
+// Get progress bar width
+func progressBarWidth() int {
+	reserved := len("00:00:00 []")
+	w := getTerminalWidth() - reserved
+	if w < 10 {
+		return 10
+	}
+	return w
+}
+
+func renderProgressBar(elapsed, total time.Duration, width int) string {
+	if total <= 0 {
+		return strings.Repeat("█", width)
+	}
+	progress := float64(elapsed) / float64(total)
+	if progress > 1 {
+		progress = 1
+	}
+	filled := int(progress * float64(width))
+	return strings.Repeat("█", filled) + strings.Repeat(" ", width-filled)
+}
 
 func main() {
 	if len(os.Args) < 2 {
-		fmt.Println("Usage: ./rdg <duration>\nExample: ./rdg 1h2m3s")
+		fmt.Println("Usage: ./rdg <duration>")
+		fmt.Println("Example: ./rdg 1h2m3s")
 		os.Exit(1)
 	}
 
-	// Load config from ~/.config/rdg/rdgconf.json
+	total, err := time.ParseDuration(os.Args[1])
+	if err != nil {
+		panic(err)
+	}
+
 	home, err := os.UserHomeDir()
 	if err != nil {
 		panic(err)
@@ -24,36 +67,46 @@ func main() {
 	configPath := filepath.Join(home, ".config", "rdg", "rdgconf.json")
 	conf := config.LoadConfig(configPath)
 
-	dur, err := time.ParseDuration(os.Args[1])
-	if err != nil {
-		panic(err)
-	}
+	fmt.Print("\033[?1049h")
+	fmt.Print("\033[2J\033[H")
+	defer func() {
+		fmt.Print("\033[?25h")
+		fmt.Print("\033[?1049l")
+	}()
 
-	ticker := time.NewTicker(time.Second)
+	start := time.Now()
+	ticker := time.NewTicker(50 * time.Millisecond)
 	defer ticker.Stop()
 
-	remaining := dur
+	for {
+		elapsed := time.Since(start)
+		remaining := total - elapsed
 
-	for remaining >= 0 {
-		h := int(remaining.Hours())
-		m := int(remaining.Minutes()) % 60
-		s := int(remaining.Seconds()) % 60
+		if remaining <= 0 {
+			break
+		}
 
-		fmt.Print("\033[3J\033[H\033[2J")
-		fmt.Printf("\r%02d:%02d:%02d", h, m, s)
+		secs := int(math.Ceil(remaining.Seconds()))
+		h := secs / 3600
+		m := (secs % 3600) / 60
+		s := secs % 60
+
+		barWidth := progressBarWidth()
+		bar := renderProgressBar(elapsed, total, barWidth)
+
+		fmt.Printf("\r%02d:%02d:%02d [%s]", h, m, s, bar)
+
 		<-ticker.C
-		remaining -= time.Second
 	}
 
-	fmt.Print("\033[3J\033[H\033[2J")
-	fmt.Println("\rDone")
+	barWidth := progressBarWidth()
+	fmt.Printf("\r00:00:00 [%s]\n", renderProgressBar(total, total, barWidth))
 
 	if conf.SoundFilePath != "" {
 		repetition := conf.Repetition
 		if repetition <= 0 {
-			repetition = 3 // temporary default
+			repetition = 3
 		}
-
 		for range repetition {
 			sound.PlaySound(conf.SoundFilePath)
 		}
